7a71e0332687cf136e4e2ce09e06146d5aa4e25e
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-26 23:54:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.collect do |call|
+    if call.
 end
 
 def find_the_cheese# code an argument here
