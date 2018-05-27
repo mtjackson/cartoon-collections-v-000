a14a0992486d60ffeb8945c46484981ca9213f22
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 00:00:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
     call_length = call.split(' ')
     if call_length.length > 4
       return true
-    elsif call_length.length < 4
+    elsif call_length.length <= 4
       return false
     end
   end
