af129a928992579c211e8cba2767c1ec01203c4a
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 00:08:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map do |name|
-    name = name.capitalize + "!"
-  end
+  array.map { |name| name = name.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
