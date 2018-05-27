2ad8e1f8a0e0bdeaf9ab11b29dc35bb34dd81624
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 00:15:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   array.collect do |maybe_cheese|
-    if array.include?(cheese_types[i])
+    if array.include?(cheese_types[0]) || array.include?(cheese_types[1]) || array.include?(cheese_types[2])
       return maybe_cheese
     end
     i += 1
